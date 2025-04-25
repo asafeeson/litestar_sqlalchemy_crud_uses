bd2b3965b4d0fd135a3d52ba2e45f9f7a2adc414
asafeeson <g.kochergin@hotmail.com>
2025-04-26 01:35:42 +0200

    - init user controller

--- api_app/api/controllers/user_controller.py
+++ api_app/api/controllers/user_controller.py
@@ -1,31 +1,45 @@
 from litestar import Controller, get, post, patch, delete
-from domain.schemas.user_schemas import UserDTO, UserCreateDTO, UserResponseDTO, UserUpdateDTO
+from domain.schemas.user_schemas import (
+    UserDTO,
+    UserCreateDTO,
+    UserResponse,
+    UserUpdateDTO,
+)
+from api_app.services.user_service import UserService
+from advanced_alchemy.filters import FilterTypes
+from advanced_alchemy.service import OffsetPagination
 
 
 class UserController(Controller):
     path = "/users"
-    tags = "Users"
-    
+    tags = ["Users"]
+
     @post("/")
-    async def create_user() -> UserResponseDTO:
-        ...
-        
+    async def create_user(
+        self, user_service: UserService, data: UserCreateDTO
+    ) -> UserResponse:
+        obj = await user_service.create(data)
+        return user_service.to_schema(data=obj, schema_type=UserResponse)
+
     @get("/")
-    async def list_users() -> list[UserResponseDTO]:
-        ...
-        
-        
+    async def list_users(
+        self, user_service: UserService, filters: list[FilterTypes]
+    ) -> OffsetPagination[UserResponse]:
+        results, total = await user_service.list_and_count(*filters)
+        return user_service.to_schema(results, total, filters, schema_type=UserResponse)
+
     @get("/{user_id:int}")
-    async def get_user(user_id: int) -> UserResponseDTO:
-        ...
-        
+    async def get_user(self, user_id: int, user_service: UserService) -> UserResponse:
+        obj = await user_service.get(user_id)
+        return user_service.to_schema(obj, schema_type=UserResponse)
+
     @patch("/{user_id:int}")
-    async def update_user(user_id: int):
-        ...
-        
-        
+    async def update_user(
+        self, user_service: UserService, data: UserUpdateDTO, user_id: int
+    ) -> UserResponse:
+        obj = await user_service.update(data=data, item_id=user_id)
+        return user_service.to_schema(obj, schema_type=UserResponse)
+
     @delete("/{user_id:int}")
-    async def delete_user(user_id: int):
-        ...
-    
-    
+    async def delete_user(self, user_service: UserService, user_id: int) -> None: 
+        await user_service.delete(item_id=user_id)
