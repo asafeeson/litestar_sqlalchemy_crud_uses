7dbdbb5087596ea21728e430e411cd3ce63f1be4
asafeeson <g.kochergin@hotmail.com>
2025-04-26 01:18:10 +0200

    feature: - add alchemy plugin and router

--- api_app/main.py
+++ api_app/main.py
@@ -2,33 +2,32 @@ import os
 from litestar import Litestar, get
 from litestar_granian import GranianPlugin
 from dotenv import load_dotenv
+from core.config import alchemy
+from api.routers import api_router
 
 load_dotenv()
 
+# DEBUG to False in Production!!!
 LITESTAR_DEBUG = bool(os.getenv("LITESTAR_DEBUG", False))
+
 
 @get("/")
 async def hello_world() -> dict[str, str]:
     return {"message": "Hello World!"}
 
 
-async def on_startup(app: Litestar) -> None:
-    ...
-    
+async def on_startup(app: Litestar) -> None: ...
+
+
 async def on_shutdown() -> None:
     # close connections
     ...
 
 
-
 app = Litestar(
-    plugins=[GranianPlugin()],
+    plugins=[GranianPlugin(), alchemy],
     on_startup=[on_startup],
     on_shutdown=[on_shutdown],
     debug=LITESTAR_DEBUG,
-    route_handlers=[
-        hello_world
-    ]
+    route_handlers=[api_router],
 )
-
-# DEBUG to False in Production!!!
